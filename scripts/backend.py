25ff57fa9b97107eefd65ea35c21e9bd92b735e0
LRuocco22 <l.ruocco22@studenti.unisa.it>
2024-12-13 23:14:19 +0100

    wip: error fixing

--- scripts/backend.py
+++ scripts/backend.py
@@ -5,7 +5,7 @@ import pickle
 
 # Settaggio dei dati per l'accesso a TMDb
 TMDB_API_KEY = "89902622f730bc1e37ef7268bdebd0e0"
-TMDB_BASE_URL = "https://api.themoviedb.org/3"
+TMDB_BASE_URL = ""
 
 # Selezione dei file del modello precedente generato che useremo per generare le raccomandazioni
 movies_path = "processed_movies.csv"
