ecf6a268273e311ddf533a44b05fd7f1060ed741
LRuocco22 <l.ruocco22@studenti.unisa.it>
2025-01-06 12:04:25 +0100

    add: try_me.py

---
+++ scripts/try_me.py
@@ -0,0 +1,27 @@
+import requests
+
+def effettua_richiesta_post(url, dati):
+    try:
+        print(f"Inviando dati a {url}: {dati}")
+        risposta = requests.post(url, json=dati)
+        
+        if risposta.status_code == 200:
+            print("Richiesta completata con successo!")
+            dati_risposta = risposta.json()
+            
+            # Stampa ogni film su una linea separata
+            for film in dati_risposta:
+                print(f"- ID: {film['tmdbId']}, Titolo: {film['title']}, Cluster: {film['cluster']}, Generi: {film['genres']}")
+        else:
+            print(f"Errore nella richiesta: {risposta.status_code}")
+            print("Dettagli:", risposta.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Errore durante la richiesta: {e}")
+
+url_endpoint = "http://127.0.0.1:8000/recommend"
+
+dati_da_inviare = {
+    "tmdb_ids": [892]
+}
+
+effettua_richiesta_post(url_endpoint, dati_da_inviare)
